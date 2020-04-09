42189bcc2ab53551cd19b659df6eb06439c0f70b
tristan <tristan@upbge.org>
2020-04-10 00:02:23 +0200

    Move conversion functions in Importer

--- src/importer/dxf/importer.h
+++ src/importer/dxf/importer.h
@@ -4,22 +4,28 @@
 
 #include <string>
 
+#include <libdxfrw/drw_entities.h>
+
 namespace Importer::Dxf
 {
 
 class Importer
 {
-	friend void operator<<(Importer &importer, const Geometry::Polyline::List &polylines);
-
 private:
 	Geometry::Polyline::List m_polylines;
+
+	void addPolyline(const Geometry::Polyline &polyline);
 
 public:
 	explicit Importer(const std::string &filename);
 
 	Geometry::Polyline::List &&polylines();
-};
 
-void operator<<(Importer &importer, const Geometry::Polyline::List &polylines);
+	void convertToPolylines(const DRW_Line &line);
+	void convertToPolylines(const DRW_Spline &spline);
+	void convertToPolylines(const DRW_LWPolyline &lwpolyline);
+	void convertToPolylines(const DRW_Circle &circle);
+	void convertToPolylines(const DRW_Arc &arc);
+};
 
 }
