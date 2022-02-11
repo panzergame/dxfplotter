6da48bc42a842845af397ff13ef0adc6130a1a20
tristan <tristan.porteries@gmail.com>
2022-02-11 09:42:06 +0100

    chore: use a script for windows deploying

---
+++ ci/deploywindows.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -x
+set -e
+
+# generate release name
+COMMIT=$(git rev-parse --short HEAD)
+TAG=$(git describe --tags)
+RELEASE_NAME="dxfplotter-$TAG-$COMMIT-x86_64"
+
+BUILD_DIR=build
+BINARY="${BINARY}Release/dxfplotter.exe"
+$DEPLOY_DIR="${RELEASE_NAME}"
+
+windeployqt --dir $DEPLOY_DIR $BINARY
+
+
+zip -r "${RELEASE_NAME}.zip" $RELEASE_NAME
