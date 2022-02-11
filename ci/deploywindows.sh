07293d5500d97ddf3d441a7d8b6fb23bdc6f9c4e
tristan <tristan.porteries@gmail.com>
2022-02-11 09:52:47 +0100

    fix: window deploy script

--- ci/deploywindows.sh
+++ ci/deploywindows.sh
@@ -8,9 +8,9 @@ COMMIT=$(git rev-parse --short HEAD)
 TAG=$(git describe --tags)
 RELEASE_NAME="dxfplotter-$TAG-$COMMIT-x86_64"
 
-BUILD_DIR=build
+BUILD_DIR="build"
 BINARY="${BINARY}Release/dxfplotter.exe"
-$DEPLOY_DIR="${RELEASE_NAME}"
+DEPLOY_DIR="${RELEASE_NAME}"
 
 windeployqt --dir $DEPLOY_DIR $BINARY
 
