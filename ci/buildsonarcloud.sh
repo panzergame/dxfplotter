94a2766bbfa6daa6e879d76bc75b02c98b46a686
tristan <tristan.porteries@gmail.com>
2025-07-25 17:04:54 +0200

    feat: update docker for qt6

--- ci/buildsonarcloud.sh
+++ ci/buildsonarcloud.sh
@@ -30,7 +30,7 @@ pushd "$BUILD_DIR"
 
 # configure build files with CMake
 # we need to explicitly set the install prefix, as CMake's default is /usr/local for some reason...
-cmake "$REPO_ROOT" -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON
+cmake "$REPO_ROOT" -DCMAKE_BUILD_TYPE=Debug -DENABLE_COVERAGE=ON -DQt6_DIR=/opt/qt/
 
 # Wraps the compilation with the Build Wrapper to generate configuration (used
 # later by the SonarQube Scanner) into the "bw-output" folder
