cffe835dd449557e18058c4722cd9426be0da761
tristan <tristan.porteries@gmail.com>
2025-07-25 17:26:08 +0200

    feat: use Qt6_DIR for cmake build

--- ci/buildappimage.sh
+++ ci/buildappimage.sh
@@ -35,7 +35,7 @@ pushd "$BUILD_DIR"
 
 # configure build files with CMake
 # we need to explicitly set the install prefix, as CMake's default is /usr/local for some reason...
-cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=OFF
+cmake "$REPO_ROOT" -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTING=OFF -DQt6_DIR=/opt/qt/6.8.2/gcc_64/lib/cmake/Qt6
 
 # build project and install files into AppDir
 make -j$(nproc)
