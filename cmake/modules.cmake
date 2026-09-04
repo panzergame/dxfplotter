9a0882565a21e7ef62717f70fee2acafb3fe132f
Tristan Porteries <tristan.porteries@nperf.com>
2026-09-04 15:20:54 +0200

    feat(module): prepare C++ module

---
+++ cmake/modules.cmake
@@ -0,0 +1,17 @@
+if (MSVC)
+    add_compile_options(/translateInclude)
+endif()
+
+function (add_module_library lib_name)
+    cmake_parse_arguments(AML "SHARED" "" "NON_MODULE_SOURCES" ${ARGN})
+    set(sources ${AML_UNPARSED_ARGUMENTS})
+
+    if (AML_SHARED)
+        add_library(${lib_name} SHARED ${AML_NON_MODULE_SOURCES})
+    else()
+        add_library(${lib_name} STATIC ${AML_NON_MODULE_SOURCES})
+    endif()
+
+    # BASE_DIRS is configured to allow files coming from src and build directory (for instance compile generated sources located in build/).
+    target_sources(${lib_name} PUBLIC FILE_SET cxx_modules TYPE CXX_MODULES FILES ${sources} BASE_DIRS ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
